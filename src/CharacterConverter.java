71c14ef74222987adba5a1e51a4403f19d62528a
rumeethundal <rumeethundal03@gmail.com>
2024-01-13 15:39:39 -0800

    implemented exit option

--- src/CharacterConverter.java
+++ src/CharacterConverter.java
@@ -6,11 +6,19 @@ public class CharacterConverter {
         Scanner input = new Scanner(System.in);
 
         while(true) {
-            System.out.println("Enter your desired character to find its frequency:");
+
+            System.out.println("Enter your desired string to see the frequency of your character provided above or\n" +
+                    "enter 'exit' to quit program: ");
+            String givenString = input.nextLine();
+            if(givenString.equals("exit")) {
+                System.out.print("You are exiting the program. Goodbye!");
+                return;
+            }
+
+            System.out.println("Enter your desired character to find its frequency: ");
             char givenChar = input.nextLine().charAt(0);
 
-            System.out.println("Enter your desired string to see the frequency of your character provided above: ");
-            String givenString = input.nextLine();
+
 
             int frequency = 0;
             for (int i = 0; i < givenString.length(); i++) {
