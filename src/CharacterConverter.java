4a07fc1a1d0903a8091707b38b261ccb578dbbee
rumeethundal <rumeethundal03@gmail.com>
2024-01-13 15:30:37 -0800

    created loop for program

--- src/CharacterConverter.java
+++ src/CharacterConverter.java
@@ -5,20 +5,22 @@ public class CharacterConverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter your desired character to find its frequency:");
-        char givenChar = input.nextLine().charAt(0);
+        while(true) {
+            System.out.println("Enter your desired character to find its frequency:");
+            char givenChar = input.nextLine().charAt(0);
 
-        System.out.println("Enter your desired string to see the frequency of your character provided above: ");
-        String givenString = input.nextLine();
+            System.out.println("Enter your desired string to see the frequency of your character provided above: ");
+            String givenString = input.nextLine();
 
-        int frequency = 0;
-        for(int i=0; i < givenString.length(); i++) {
-            if(givenChar == givenString.charAt(i)) {
-                frequency++;
+            int frequency = 0;
+            for (int i = 0; i < givenString.length(); i++) {
+                if (givenChar == givenString.charAt(i)) {
+                    frequency++;
+                }
             }
+
+            System.out.println("The character " + givenChar + " appears " + frequency + " times in the provided string");
+
         }
-
-        System.out.println("The character " + givenChar + " appears " + frequency + " times in the provided string");
-
     }
 }
