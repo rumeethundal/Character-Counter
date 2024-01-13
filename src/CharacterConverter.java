84e9e1cebe2fc2047504fde0e6dd0c0701434e86
rumeethundal <rumeethundal03@gmail.com>
2024-01-13 11:21:26 -0800

    created prompt for server

--- src/CharacterConverter.java
+++ src/CharacterConverter.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CharacterConverter {
+import java.util.Scanner;
+
+public class CharacterConverter {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Enter your desired string:");
+        String givenString = kb.nextLine();
+
+        System.out.println("Enter the character to see its frequency in the given string: ");
+        String givenCharacter = kb.nextLine();
+
+
+
+
+    }
 }
