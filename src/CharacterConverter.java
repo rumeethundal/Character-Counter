4caec81c96aa44e6d9d362edb5a8fe4e08e15c33
rumeethundal <rumeethundal03@gmail.com>
2024-01-13 13:08:10 -0800

    created loop to keep track of frequency and prompted out answer for user

--- src/CharacterConverter.java
+++ src/CharacterConverter.java
@@ -5,14 +5,21 @@ public class CharacterConverter {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        System.out.println("Enter your desired string:");
-        String givenString = kb.nextLine();
+        System.out.println("Enter your desired character to find its frequency:");
+        String givenChar = kb.nextLine();
 
-        System.out.println("Enter the character to see its frequency in the given string: ");
-        String givenCharacter = kb.nextLine();
+        System.out.println("Enter your desired string to see the frequency of your character provided above: ");
+        char givenString = kb.next().charAt(0);
 
+        int frequency = 0;
+        for(int i=0; i < givenChar.length(); i++) {
+            if(givenChar.charAt(i) == givenString) {
+                frequency++;
+            }
+        }
 
-
+        System.out.println("The character " + givenChar + " appears " + frequency + " in the provided string");
 
     }
 }
+
