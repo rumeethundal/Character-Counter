e7f7a6dd4c5a609d658abcc74b6c7c6cf5a0d36c
rumeethundal <rumeethundal03@gmail.com>
2024-01-15 15:16:02 -0800

    implemented error for more than one character

--- src/CharacterConverter.java
+++ src/CharacterConverter.java
@@ -9,25 +9,30 @@ public class CharacterConverter {
 
             System.out.println("Enter your desired string to see the frequency of your character provided above or\n" +
                     "enter 'exit' to quit program: ");
-            String givenString = input.nextLine();
-            if(givenString.equals("exit")) {
+            String stringToCount = input.nextLine();
+            if(stringToCount.equals("exit")) {
                 System.out.print("You are exiting the program. Goodbye!");
                 return;
             }
 
             System.out.println("Enter your desired character to find its frequency: ");
-            char givenChar = input.nextLine().charAt(0);
+            String freqString = input.nextLine();
+            if(freqString.length() != 1) {
+                System.out.println("Invalid. Please get one character");
+                continue;
+            }
 
-
+            char freqChar;
+            freqChar = freqString.charAt(0);
 
             int frequency = 0;
-            for (int i = 0; i < givenString.length(); i++) {
-                if (givenChar == givenString.charAt(i)) {
+            for (int i = 0; i < stringToCount.length(); i++) {
+                if (freqChar == stringToCount.charAt(i)) {
                     frequency++;
                 }
             }
 
-            System.out.println("The character " + givenChar + " appears " + frequency + " times in the provided string");
+            System.out.println("The character " + freqChar + " appears " + frequency + " times in the provided string");
 
         }
     }
